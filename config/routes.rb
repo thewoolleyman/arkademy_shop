b370b74ac13cec6f49eb78e1d0c3eef734707892
Chad Woolley <cwoolley@gitlab.com>
2022-02-21 16:02:40 -0600

    add admin layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :products
+  end
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
